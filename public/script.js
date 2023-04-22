890ea0d4720cf7ad0314ee17f839b00448b465f0
Yash1234567891011 <73171838+Yash1234567891011@users.noreply.github.com>
2023-04-22 18:29:43 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -7,6 +7,22 @@ var peer = new Peer(undefined, {
 });
 
 const user = prompt("Enter your name");
+const myVideo=document.createElement("video")
+myVideo.muted=true
+let mystream
+navigator.mediaDevices.getUserMedia({
+    audio:true,video:true
+}).then((stream)=>{
+    mystream=stream
+    addVideoStream(myVideo,stream)
+})
+function addVideoStream(video,stream){
+    video.srcObject=stream
+    video.addEventListener("loadedmetadata",()=>{
+        video.play()
+        $("video_grid").append(video)
+    })
+}
 
 $(function () {
     $("#show_chat").click(function () {
